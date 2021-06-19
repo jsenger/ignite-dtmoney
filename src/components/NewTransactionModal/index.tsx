2251f5702417851f4d2383833cc87c1fc4e6bf3c
jéssica senger <jessicasbogoni1@gmail.com>
2021-06-19 15:30:21 -0400

    Close modal after transaction creation

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -25,15 +25,17 @@ export function NewTransactionModal({
   const [category, setCategory] = useState('');
   const [type, setType] = useState('deposit');
 
-  function handleCreateNewTransaction(event: FormEvent) {
+  async function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
 
-    createTransaction({
+    await createTransaction({
       title,
       amount,
       category,
       type,
     });
+
+    onRequestClose();
   }
 
   return (
